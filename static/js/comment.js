53eaa3beea20548c3773e544075e462387ff7fa8
E9KD <931262536@qq.comn>
2019-09-09 14:20:54 +0800

    1

---
+++ static/js/comment.js
@@ -0,0 +1,19 @@
+/*
+ * @Company: Ling Xun
+ * @Author: Song
+ * @Date: 2019-09-09 09:42:39
+ * @LastEditTime: 2019-09-09 10:18:54
+ */
+function TabBarChange(x) {
+    // switch (x) {
+    //     case 0:
+    //         window.location.href = "../../views/goodsinfo.html";
+    //         break;
+    //     case 1:
+    //         window.location.href = "../../views/buy.html";
+    //         break;
+    //     case 2:
+    //         window.location.href = "../../views/buy.html";
+    //         break;
+    // }
+}
